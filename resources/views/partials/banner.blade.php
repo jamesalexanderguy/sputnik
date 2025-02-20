3f5ce7d0731a2112bc267bd9d405a11d033da09a
James Guy <jimmy@spaceracedigital.com>
2025-02-20 08:00:14 -0500

    Add portfolio section and clients

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -4,29 +4,11 @@
         <div class="blur-[106px] h-32 bg-gradient-to-r from-sky-100 to-secondary dark:to-indigo-600"></div>
     </div>
     <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
-        <div class="relative pt-36 ml-auto">
+        <div class="relative py-36 ml-auto">
             <div class="lg:w-2/3 text-center mx-auto">
-            <h1 class="dark:text-white font-bold text-5xl md:text-6xl xl:text-7xl"><span class="text-primary dark:text-white">Light up</span> your corner of the galaxy.</h1>
-            <!--<p class="mt-8 text-gray-700 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt nam itaque sed eius modi error totam sit illum. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum soluta!</p>-->
-                <div class="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
-                    <a
-                      href="#"
-                      class="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
-                    >
-                      <span class="relative text-base font-semibold text-white"
-                        >Make Contact</span
-                      >
-                    </a>
-                    <a
-                      href="#contact"
-                      class="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
-                    >
-                      <span
-                        class="relative text-base font-semibold text-primary dark:text-white"
-                        >Learn more</span
-                      >
-                    </a>
-                </div>
+            <h1 class="font-bold text-5xl md:text-6xl xl:text-7xl"><span class="text-primary dark:text-white">Light up</span> your corner of the galaxy</h1>
+            <h2 class="text-3xl mt-8 text-gray-700">Websites and campaigns to get your message home.</h2>
+                
                 
         </div>
 </div>
---
+++ resources/views/partials/portfolio.blade.php
@@ -0,0 +1,167 @@
+<div id="features">
+  <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+    <div class="mx-auto md:w-2/3 lg:w-1/2">
+      <h2 class="my-8 text-4xl font-bold text-gray-700 text-center md:text-5xl">
+      The <span class="text-primary">features</span> you need.
+      All in one place.
+      </h2>
+    </div>
+
+    <!--Osprey Community Foundation-->
+    <section class="container mt-14 mb-24 md:my-16 lg:my-18 2xl:mt-0 2xl:mb-30">
+      <div class="grid gap-4 md:grid-cols-2 2xl:gap-6">
+        <!--block one-->
+        <div class="group px-6 py-8 order-3 text-white overflow-hidden rounded-lg bg-[#cd6557] md:order-1 lg:row-span-2">
+          <h2 class="text-4xl font-bold mb-6">Osprey Community Foundation</h2>
+          <p class="mb-6">The wilderness ski experience of a lifetime, in the epic terrain of the Purcell Mountains. We built this WordPress website, implemented the e-commerce system and provide ongoing site support, maintenance and security.</p>
+          <p class="mb-6">We collaborated with The Forest for site architecture, UX, strategy and design.</p>
+          <a href="https://ospreycommunityfoundation.ca/" class="max-w-80 flex items-center justify-between group-hover:text-primary">
+            <span class="text-sm">Visit Osprey Community Foundation</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+              <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z" clip-rule="evenodd" />
+            </svg>                
+          </a>
+
+
+          
+        
+        </div>
+
+      
+
+        
+        <!--block two-->
+        <div class="relative h-96 overflow-hidden border border-gray-200 order-1 flex flex-col items-start rounded-lg bg-secondary md:order-3 md:col-span-2 lg:order-2 lg:col-span-1">
+          <div class="shadow-xl rounded-lg" style="transform-origin: right center;">
+            <div class="flex items-center pl-3 space-x-1 bg-gray-200 rounded-t-lg h-7">
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+            </div>
+
+            <div class="overflow-auto h-96">
+              <img class="w-full top-7" alt="Osprey Community Foundation" loading="lazy" src="/wp-content/themes/sputnik/resources/images/screencapture-ospreycommunityfoundation-ca-2025-02-20-07_05_59-min.jpg">
+            </div>
+          </div>
+        
+          
+          
+        </div>
+        <!--block three-->
+        <div class="overflow-hidden order-2 flex h-full flex-col rounded-lg bg-tertiary text-white lg:order-3">
+          
+          <img class="w-full object-cover" alt="Selkirk Snowcat Skiing staff gallery page" loading="lazy" src="/wp-content/themes/sputnik/resources/images/osprey-search-funds.jpg">
+
+        </div>
+      </div>
+    </section>
+    <!--end Osprey-->
+
+
+    <!--Selkirk Snowcat Skiing-->
+    <section class="container mt-14 mb-24 md:my-16 lg:my-18 2xl:mt-0 2xl:mb-30">
+      <div class="grid gap-4 md:grid-cols-2 2xl:gap-6">
+        <!--block one-->
+        <div class="group px-6 py-8 order-3 text-white overflow-hidden rounded-lg bg-[#5b98c1] md:order-1 lg:row-span-2">
+          <h2 class="text-4xl font-bold mb-6">Selkirk Snowcat Skiing</h2>
+          <p class="mb-6">The wilderness ski experience of a lifetime, in the epic terrain of the Purcell Mountains. We built this WordPress website, implemented the e-commerce system and provide ongoing site support, maintenance and security.</p>
+          <p class="mb-6">We collaborated with The Forest for site architecture, UX, strategy and design.</p>
+          <a href="https://selkirksnowcatskiing.com/" class="max-w-80 flex items-center justify-between group-hover:text-primary">
+            <span class="text-sm">Visit Selkirk Snowcat Skiing</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+              <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z" clip-rule="evenodd" />
+            </svg>                
+          </a>
+
+
+          <img class="w-full mt-6 rounded-lg" alt="Selkirk Snowcat Skiing snow report app" loading="lazy" src="/wp-content/themes/sputnik/resources/images/snow-report.jpg">
+        
+        </div>
+
+        
+        <!--block two-->
+        <div class="relative h-96 overflow-hidden border border-gray-200 order-1 flex flex-col items-start rounded-lg bg-secondary md:order-3 md:col-span-2 lg:order-2 lg:col-span-1">
+          <div class="shadow-xl rounded-lg" style="transform-origin: right center;">
+            <div class="flex items-center pl-3 space-x-1 bg-gray-200 rounded-t-lg h-7">
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+            </div>
+
+            <div class="overflow-auto h-96">
+              <img class="w-full top-7" alt="Selkirk Snowcat Skiing" loading="lazy" src="/wp-content/themes/sputnik/resources/images/screencapture-selkirksnowcatskiing-why-selkirk-2025-02-19-18_39_46-min.jpg">
+            </div>
+          </div>
+        
+          
+          
+        </div>
+        <!--block three-->
+        <div class="overflow-hidden order-2 flex h-full flex-col rounded-lg bg-tertiary text-white lg:order-3">
+          
+          <img class="w-full object-cover" alt="Selkirk Snowcat Skiing staff gallery page" loading="lazy" src="/wp-content/themes/sputnik/resources/images/sss-team.jpg">
+
+        </div>
+      </div>
+    </section>
+    <!--end Selkirk Snowcat Skiing-->
+
+    <!--Greenpeace-->
+    <section class="container mt-14 mb-24 md:my-16 lg:my-18 2xl:mt-0 2xl:mb-30">
+      <div class="grid gap-4 md:grid-cols-2 2xl:gap-6">
+        <!--block one-->
+        <div class="flex order-3 overflow-hidden rounded-lg bg-tertiary md:order-1 lg:row-span-2">
+          
+          <img class="w-full object-cover" alt="Greenpeace" loading="lazy" src="/wp-content/themes/sputnik/resources/images/65465335gp.jpg">
+        </div>
+        <!--block two-->
+        <div class=" order-1 flex flex-col items-start rounded-lg bg-secondary md:order-3 md:col-span-2 lg:order-2 lg:col-span-1">
+          <img class="w-full" alt="Greenpeace" loading="lazy" src="https://spaceracedigital.com/wp-content/uploads/2024/05/people-vs-oil.svg">
+        </div>
+        <!--block three-->
+        <div class="group order-2 flex h-full flex-col rounded-lg bg-[#88c449] px-6 py-8 text-white lg:order-3">
+          <h2 class="text-4xl font-bold mb-6">Greenpeace</h2>
+          <p class="mb-6">The People VS Oil was a Greenpeace Canada initiative to call attention to the damage Big Oil does to the marine environment of the West Coast of Canada, and to tell the world about the impact on indigenous communities in the area.</p>
+          <p class="mb-6">SpaceRace Digital was responsible for campaign marketing efforts, managed post publication and was responsible for technical support on the website, custom CMS, and mapping application during the campaign.</p>
+          <a href="https://www.greenpeace.org/international/campaigns/people-vs-oil/" class="max-w-80 flex items-center justify-between group-hover:text-primary">
+            <span class="text-sm">Visit Greenpeace</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+              <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z" clip-rule="evenodd" />
+            </svg>                
+          </a>
+        </div>
+      </div>
+    </section>
+    <!--end Greenpeace-->
+
+    <!--UC Davis-->
+    <section class="container mt-14 mb-24 md:my-16 lg:my-18 2xl:mt-0 2xl:mb-30">
+      <div class="grid gap-4 md:grid-cols-2 2xl:gap-6">
+        <!--block one-->
+        <div class="flex order-3 overflow-hidden rounded-lg bg-tertiary md:order-1 lg:row-span-2">
+          
+          <img class="w-full object-cover" alt="UC Davis Immigrant Legal Services Toolkit" loading="lazy" src="/wp-content/themes/sputnik/resources/images/987789798797.jpg">
+        </div>
+        <!--block two-->
+        <div class=" order-1 flex flex-col items-start rounded-lg bg-secondary md:order-3 md:col-span-2 lg:order-2 lg:col-span-1">
+          <img class="w-full" alt="UC Davis Immigrant Legal Services Toolkit" loading="lazy" src="https://spaceracedigital.com/wp-content/uploads/2021/05/ucimm.svg">
+        </div>
+        <!--block three-->
+        <div class="order-2 flex h-full flex-col rounded-lg bg-[#4d97cf] px-6 py-8 text-white lg:order-3">
+          <h2 class="text-4xl font-bold mb-6">UC Davis Immigrant Legal Services Toolkit</h2>
+            <p class="mb-6">UC Davis School of Law needed a portal in which to share their vast wealth of legal resources for immigrants and undocumented students with other higher education establishments across the United States.</p>
+            <p class="mb-6">We worked closely with the UC Davis team and the NexGenAmerica funding team to create the site architecture, UX, strategy and design, and i built the custom WordPress website with paywall and member management system.</p>  
+        
+        
+        </div>
+      </div>
+    </section>
+    <!--End UC Davis Law-->
+
+
+
+  
+
+
+  </div>
+</div>
