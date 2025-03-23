e908f7ea9968b4e47393ae44f63de10246815639
James Guy <admin@Jamess-MacBook-Pro.local>
2025-03-22 19:11:22 -0500

    Change config url

--- bud.config.js
+++ bud.config.js
@@ -34,7 +34,7 @@ export default async (app) => {
    */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('http://vostok.test')
+    .setProxyUrl('http://allset.test')
     .watch(['resources/views', 'app']);
 
   /**
