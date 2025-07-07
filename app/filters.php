d759b4e8b343b0ce631d4699bb466f6c2fc0d57d
James Guy <admin@Jamess-MacBook-Pro.local>
2025-07-07 15:06:45 -0500

    Override content so no content box on pages

--- app/filters.php
+++ app/filters.php
@@ -14,3 +14,7 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+add_filter('use_block_editor_for_post_type', function ($use_block_editor, $post_type) {
+    return $post_type === 'page' ? false : $use_block_editor;
+}, 10, 2);
