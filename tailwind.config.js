11d37de185084733e18edc48208a99257e399fb7
James Guy <admin@Jamess-MacBook-Pro.local>
2025-07-07 15:04:28 -0500

    Add default styles and theme variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ const config = {
   content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
   theme: {
     extend: {
-      colors: {}, // Extend Tailwind's default colors
+      colors: {
+        overlaycolor: '#7f7f7f',
+        copyrightcolor: '#E6E6E6',
+        hovercolor: '#EEE444',
+        linkcolor: '#F0F0F0',
+      }, 
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
     },
   },
   plugins: [],
