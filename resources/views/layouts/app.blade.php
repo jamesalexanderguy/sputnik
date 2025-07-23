9bbd2485d02da1049396d4c61952022c2d1122f1
James Guy <admin@Jamess-MacBook-Pro.local>
2025-07-23 18:30:11 -0500

    Polish app base with Alpine and tailwind extras

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html @php(language_attributes())>
+<html @php(language_attributes()) x-data="{ mobileMenuOpen: false }" class="scroll-smooth antialiased text-gray-900 bg-white">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,23 +7,25 @@
     @php(wp_head())
   </head>
 
-  <body class="{{ implode(' ', get_body_class()) }}">
+  <body class="{{ implode(' ', get_body_class()) }} font-sans leading-relaxed tracking-wide">
     @php(wp_body_open())
 
-    <div id="app">
-      <a class="sr-only focus:not-sr-only" href="#main">
+    <div id="app" class="min-h-screen flex flex-col">
+      <a href="#main" class="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 bg-blue-600 text-white px-4 py-2 rounded">
         {{ __('Skip to content') }}
       </a>
 
       @include('sections.header')
 
-      <main id="main" class="main">
+      <main id="main" class="flex-grow" role="main">
         @yield('content')
       </main>
 
       @hasSection('sidebar')
-        <aside class="sidebar">
-          @yield('sidebar')
+        <aside class="w-full bg-gray-100 border-t border-gray-200">
+          <div class="container mx-auto px-4 py-6">
+            @yield('sidebar')
+          </div>
         </aside>
       @endif
 
