5fe61a35e2d4cc001cd6be539a5b258aa7367bd8
James Guy <jimmy@spaceracedigital.com>
2025-07-29 16:27:39 -0500

    Add newest portfolio pieces

--- resources/views/partials/portfolio.blade.php
+++ resources/views/partials/portfolio.blade.php
@@ -5,6 +5,164 @@
         We've launched some</br><span class="text-primary">epic</span> projects.</h2>
     </div>
 
+<!--Plasticspact-->
+<section class="mx-auto container mt-14 mb-24 md:my-16 lg:my-18 2xl:mt-0 2xl:mb-30">
+      <div class="grid gap-4 md:grid-cols-2 2xl:gap-6 content-stretch">
+
+        <!--block one-->
+        <div class="flex order-3 overflow-hidden rounded-lg bg-tertiary md:order-1 lg:row-span-2">
+          <img class="w-full object-cover" width="608" height="953" alt="Woman buying granola from the bulk section" src="/wp-content/themes/sputnik/resources/images/Screenshot 2025-07-28 at 4.23.33 PM-min.jpg">
+        </div>
+
+        <!--block two-->
+        <div class="relative order-1 flex flex-col items-start rounded-lg overflow-hidden bg-secondary md:order-3 md:col-span-2 lg:order-2 lg:col-span-1">
+          <div class="w-full z-10 absolute top-0 shadow-xl" style="transform-origin: right center;">
+            <div class="flex items-center pl-3 space-x-1 bg-gray-200 rounded-t-lg h-7">
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+            </div>
+          </div>
+          <div class="w-full pt-[65%] md:pt-[85%] lg:pt-[65%] h-full overflow-hidden minibrowser relative border border-gray-200">
+            <img class="superscroll absolute h-fit w-full" height="1816" width="602" alt="Canada Plastics Pact website demo" src="/wp-content/themes/sputnik/resources/images/screencapture-plasticspact-sm.jpg">
+          </div>
+        </div>
+        <!--block three-->
+        <div class="group order-2 flex grow-1 h-full flex-col rounded-lg bg-[#f0f0f0] px-6 py-8 lg:order-3">
+          <h2 class="text-4xl text-[#1D4551] font-bold mb-6">Canada Plastics Pact</h2>
+          <p class="mb-6">The Canada Plastics Pact brings together leaders from across the plastics value chain committed to eliminating waste and building
+a circular plastics economy.</p>
+          <p class="mb-6">We rebuilt the custom, responsive WordPress site in a custom Bedrock setup.</p>
+          <p class="mb-6">We built various custom ACF blocks, integrated a new language switcher with WeGlot, implemented a new resource gateway, and made extensive upgrades to the existing Twig and Timber architecture.</p>
+          <p class="mb-6">SpaceRace collaborated with the Canada Plastics Pact team on this project. Céline Cobb created the design.</p>
+          <a href="https://canadaplasticspact.ca/" target="_blank" class="max-w-80 flex items-center group-hover:text-primary">
+            <span class="visit text-sm">Visit Canada Plastics Pact</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+              <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z" clip-rule="evenodd" />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </section>
+    <!--end Plasticspact-->
+
+<!--Generate Canada-->
+<section class="mx-auto container mt-14 mb-24 md:my-16 lg:my-18 2xl:mt-0 2xl:mb-30">
+      <div class="grid gap-4 md:grid-cols-2 2xl:gap-6 content-stretch">
+
+        <!--block one-->
+        <div class="flex order-3 overflow-hidden rounded-lg bg-tertiary md:order-1 lg:row-span-2">
+          <img class="w-full object-cover" width="608" height="953" alt="Canadian power lines in sunset" src="/wp-content/themes/sputnik/resources/images/Screenshot 2025-07-29 at 10.40.07 AM-min.jpg">
+        </div>
+
+        <!--block two-->
+        <div class="relative order-1 flex flex-col items-start rounded-lg overflow-hidden bg-secondary md:order-3 md:col-span-2 lg:order-2 lg:col-span-1">
+          <div class="w-full z-10 absolute top-0 shadow-xl" style="transform-origin: right center;">
+            <div class="flex items-center pl-3 space-x-1 bg-gray-200 rounded-t-lg h-7">
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+            </div>
+          </div>
+          <img class="w-full mt-7" width="559" height="422" alt="Generate Canada website" src="/wp-content/themes/sputnik/resources/images/gencanada-screen.jpg">
+        </div>
+        <!--block three-->
+        <div class="group order-2 flex grow-1 h-full flex-col rounded-lg bg-[#f0f0f0] px-6 py-8 lg:order-3">
+          <h2 class="text-4xl text-[#470A68] font-bold mb-6">Generate Canada</h2>
+          <p class="mb-6">Generate Canada innovates impactful, scalable, and human-centered solutions to complex sustainability problems at the heart of economy, environment, and society.</p>
+          <p class="mb-6">We were hired to renovate the custom Bedrock installation.</p>
+          <p class="mb-6">We updated the existing Timber and Twig architecture and added new ACF blocks to improve the user experience and site administration.</p>
+          <a href="https://generatecanada.ca/" target="_blank" class="max-w-80 flex items-center group-hover:text-primary">
+            <span class="visit text-sm">Visit Generate Canada</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+              <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z" clip-rule="evenodd" />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </section>
+    <!--end Gencan-->
+
+    <!--Energy Futures Lab-->
+<section class="mx-auto container mt-14 mb-24 md:my-16 lg:my-18 2xl:mt-0 2xl:mb-30">
+      <div class="grid gap-4 md:grid-cols-2 2xl:gap-6 content-stretch">
+
+        <!--block one-->
+        <div class="flex order-3 overflow-hidden rounded-lg bg-white md:order-1 lg:row-span-2 p-12">
+          <img class="w-full object-cover" width="608" height="953" alt="Colourful plastic bottles stacked ready for recycling" src="/wp-content/themes/sputnik/resources/images/efl-fellowship-500x333-1-min.jpg">
+        </div>
+
+        <!--block two-->
+        <div class="relative order-1 flex flex-col items-start rounded-lg overflow-hidden bg-secondary md:order-3 md:col-span-2 lg:order-2 lg:col-span-1">
+          <div class="w-full z-10 absolute top-0 shadow-xl" style="transform-origin: right center;">
+            <div class="flex items-center pl-3 space-x-1 bg-gray-200 rounded-t-lg h-7">
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+            </div>
+          </div>
+          <div class="w-full pt-[65%] md:pt-[85%] lg:pt-[65%] h-full overflow-hidden minibrowser relative border border-gray-200">
+            <img class="superscroll absolute h-fit w-full" height="1816" width="602" alt="Energy Futures Lab website demo" src="/wp-content/themes/sputnik/resources/images/screencapture-energyfutureslab-about-the-efl-2025-07-28-16_39_58-min.jpg">
+          </div>
+        </div>
+        <!--block three-->
+        <div class="group order-2 flex grow-1 h-full flex-col rounded-lg bg-[#f0f0f0] px-6 py-8 lg:order-3">
+          <h2 class="text-4xl text-[#7ab24f] font-bold mb-6">Energy Futures Lab</h2>
+          <p class="mb-6">The Energy Futures Lab promotes innovation to accelerate the transition to a net-zero future.</p>
+          <p class="mb-6">We delivered a full website overhaul and brand refresh, transforming an outdated site into a modern, user-friendly platform that supports the EFL's evolving initiatives and partnerships. The redesign improved content structure, navigation, SEO, and audience pathways, while enhancing the existing brand through a refined logo, updated colours, and a more cohesive visual system. Built with security best practices, flexible templates, and a tailored admin experience, the site is easy for staff to manage. </p>
+          <p class="mb-6">We continue to support the team to ensure the platform remains secure, accessible, and aligned with their ongoing work.</p>
+          <p class="mb-6">SpaceRace collaborated with The Forest on this project for site architecture, UX, strategy and design.</p>
+          <a href="https://energyfutureslab.com/" target="_blank" class="max-w-80 flex items-center group-hover:text-primary">
+            <span class="visit text-sm">Visit Energy Futures Lab</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+              <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z" clip-rule="evenodd" />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </section>
+    <!--end energy futures-->
+
+ <!--Waldorf-->
+ <section class="mx-auto container mt-14 mb-24 md:my-16 lg:my-18 2xl:mt-0 2xl:mb-30">
+      <div class="grid gap-4 md:grid-cols-2 2xl:gap-6 content-stretch">
+
+        <!--block one-->
+        <div class="flex order-3 overflow-hidden rounded-lg bg-tertiary md:order-1 lg:row-span-2">
+          <img class="w-full object-cover" width="608" height="953" alt="Kid playing outside in autumn" src="/wp-content/themes/sputnik/resources/images/dragon-play.jpg">
+        </div>
+
+        <!--block two-->
+        <div class="relative order-1 flex flex-col items-start rounded-lg overflow-hidden bg-secondary md:order-3 md:col-span-2 lg:order-2 lg:col-span-1">
+          <div class="w-full z-10 absolute top-0 shadow-xl" style="transform-origin: right center;">
+            <div class="flex items-center pl-3 space-x-1 bg-gray-200 rounded-t-lg h-7">
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+              <span class="w-2 h-2 bg-white rounded-full"></span>
+            </div>
+          </div>
+          <div class="w-full pt-[65%] md:pt-[85%] lg:pt-[65%] h-full overflow-hidden minibrowser relative border border-gray-200">
+            <img class="superscroll absolute h-fit w-full" height="1816" width="602" alt="Nelson Waldorf School website demo" src="/wp-content/themes/sputnik/resources/images/nelsonwaldorf-org-site-demo-sm.jpg">
+          </div>
+        </div>
+        <!--block three-->
+        <div class="group order-2 flex grow-1 h-full flex-col rounded-lg bg-[#f0f0f0] px-6 py-8 lg:order-3">
+          <h2 class="text-4xl text-[#6e8343] font-bold mb-6">Nelson Waldorf School</h2>
+          <p class="mb-6">Waldorf education develops students who have a deep reverence for the natural world, and who have the critical thinking skills to create positive change.</p>
+          <p class="mb-6">We built a beautiful new responsive WordPress theme.</p>
+          <p class="mb-6">We created an intuitive back end experience with custom Gutenberg blocks.</p>
+          <p class="mb-6">We collaborated with The Forest on this project for site architecture, UX, strategy and design.</p>
+          <a href="https://nelsonwaldorf.org/" target="_blank" class="max-w-80 flex items-center group-hover:text-primary">
+            <span class="visit text-sm">Visit Nelson Waldorf School</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+              <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z" clip-rule="evenodd" />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </section>
+    <!--end Waldorf-->
+
     <!--Osprey Community Foundation-->
     <section class="mx-auto container mt-14 mb-24 md:my-16 lg:my-18 2xl:mt-0 2xl:mb-30">
       <div class="grid gap-4 md:grid-cols-2 2xl:gap-6 content-stretch">
@@ -23,18 +181,18 @@
               <span class="w-2 h-2 bg-white rounded-full"></span>
             </div>
           </div>
-          <div class="w-full pt-[65%] lg:pt-[45%] h-full overflow-hidden minibrowser relative border border-gray-200">
+          <div class="w-full pt-[65%] md:pt-[85%] lg:pt-[65%] h-full overflow-hidden minibrowser relative border border-gray-200">
             <img class="superscroll absolute h-fit w-full" height="1816" width="602" alt="Osprey Community Foundation" src="/wp-content/themes/sputnik/resources/images/screencapture-ospreycommunityfoundation-ca-2025-02-20-07_05_59-min.jpg">
           </div>
         </div>
         <!--block three-->
-        <div class="group order-2 flex grow-1 h-full flex-col rounded-lg bg-[#d36d5a] px-6 py-8 text-white lg:order-3">
-          <h2 class="text-4xl font-bold mb-6">Osprey Community Foundation</h2>
+        <div class="group order-2 flex grow-1 h-full flex-col rounded-lg bg-[#f0f0f0] px-6 py-8 lg:order-3">
+          <h2 class="text-4xl text-[#d36d5a] font-bold mb-6">Osprey Community Foundation</h2>
           <p class="mb-6">Nelson’s community foundation distributes grants to a variety of charitable organizations to positively impact our community. </p>
           <p class="mb-6">We built a custom, responsive WordPress site with an Ajax search application. </p>
           <p class="mb-6">We implemented the donation system using WooCommerce e-commerce.</p>
           <p class="mb-6">We provide ongoing site support, maintenance and security.</p>
-          <p class="mb-6">Spacerace collaborated with The Forest on this project for site architecture, UX, strategy and design.</p>
+          <p class="mb-6">SpaceRace collaborated with The Forest on this project for site architecture, UX, strategy and design.</p>
           <a href="https://ospreycommunityfoundation.ca/" target="_blank" class="max-w-80 flex items-center group-hover:text-primary">
             <span class="visit text-sm">Visit Osprey Community Foundation</span>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100">
@@ -66,15 +224,15 @@
             </div>
           </div>
 
-          <div class="w-full pt-[65%] md:pt-[85%] lg:pt-[45%] h-full overflow-hidden minibrowser relative border border-gray-200">
+          <div class="w-full pt-[65%] md:pt-[85%] lg:pt-[65%] h-full overflow-hidden minibrowser relative border border-gray-200">
             <img class="superscroll absolute h-fit w-full" height="2211" width="602" alt="Selkirk Snowcat Skiing" src="/wp-content/themes/sputnik/resources/images/screencapture-selkirksnowcatskiing-why-selkirk-2025-02-19-18_39_46-min.jpg">
           </div>
         </div>
         
         
         <!--block three-->
-        <div class="group order-2 flex h-full flex-col rounded-lg bg-[#438fbc] px-6 py-8 text-white lg:order-3">
-          <h2 class="text-4xl font-bold mb-6">Selkirk Snowcat Skiing</h2>
+        <div class="group order-2 flex h-full flex-col rounded-lg bg-[#f0f0f0] px-6 py-8 lg:order-3">
+          <h2 class="text-4xl text-[#438fbc] font-bold mb-6">Selkirk Snowcat Skiing</h2>
           <p class="mb-6">The wilderness ski experience of a lifetime, in the epic terrain of the Purcell Mountains.</p>
           <p class="mb-6">We built this WordPress website, implemented the e-commerce system and provide ongoing site support, maintenance and security.</p>
           <p class="mb-6">We collaborated with The Forest for site architecture, UX, strategy and design.</p>
@@ -112,8 +270,8 @@
           <img class="w-full mt-7" width="559" height="422" alt="Greenpeace People VS OIl campaign" src="/wp-content/themes/sputnik/resources/images/greenpeace-people-oil-campaign.jpg">
         </div>
         <!--block three-->
-        <div class="group order-2 flex h-full flex-col rounded-lg bg-[#64952c] px-6 py-8 text-white lg:order-3">
-          <h2 class="text-4xl font-bold mb-6">Greenpeace</h2>
+        <div class="group order-2 flex h-full flex-col rounded-lg bg-[#f0f0f0] px-6 py-8 lg:order-3">
+          <h2 class="text-4xl text-[#64952c] font-bold mb-6">Greenpeace</h2>
           <p class="mb-6">The People VS Oil was a Greenpeace Canada initiative to call attention to the damage Big Oil does to the marine environment of the West Coast of Canada, and to tell the world about the impact on indigenous communities in the area.</p>
           <p class="mb-6">SpaceRace Digital was responsible for campaign marketing efforts, managed post publication and was responsible for technical support on the website, custom CMS, and mapping application during the campaign.</p>
           <!-- <a href="https://www.greenpeace.org/international/campaigns/people-vs-oil/" class="max-w-80 flex items-center justify-between group-hover:text-primary">
@@ -146,8 +304,8 @@
           <img class="w-full mt-7" width="559" height="422" alt="UC Davis Immigrant Legal Services Toolkit" src="/wp-content/themes/sputnik/resources/images/ucimm.jpg">
         </div>
         <!--block three-->
-        <div class="order-2 flex h-full flex-col rounded-lg bg-[#3790cd] px-6 py-8 text-white lg:order-3">
-          <h2 class="text-4xl font-bold mb-6">UC Davis Immigrant Legal Services Toolkit</h2>
+        <div class="order-2 flex h-full flex-col rounded-lg bg-[#f0f0f0] px-6 py-8 lg:order-3">
+          <h2 class="text-4xl text-[#3790cd] font-bold mb-6">UC Davis Immigrant Legal Services Toolkit</h2>
           <p class="mb-6">UC Davis School of Law needed a portal in which to share their vast wealth of legal resources for immigrants and undocumented students with other higher education establishments across the United States.</p>
           <p class="mb-6">SpaceRace Digital worked closely with the UC Davis team and the NexGenAmerica funding team to create the site architecture, UX, strategy and design, and built the custom WordPress website with resource gateway and member management system.</p>
         </div>
